9e2aefec0860ae264e77658aca80690182824c17
Ibrahim Hassan <38167985+fmmido@users.noreply.github.com>
2024-08-08 08:16:58 +0300

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -12,6 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $pass = $_POST['password'];
